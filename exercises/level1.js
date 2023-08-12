3473102e2eb544d66431c5bf4e86e5efebf2e8b7
Derek Szeto <szeto.d71@gmail.com>
2023-08-12 14:07:15 -0400

    intro to js lab

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,25 +1,32 @@
 function lvl1exercise1 () {
   // Declare a variable as null and return it!
-
+let myvariable = null;
+return myvariable;
 }
+
 
 function lvl1exercise2 () {
   // Declare a variable with a number value and return it
-
+let myvariable2 = 3;
+return myvariable2;
 }
 
 function lvl1exercise3 () {
   // Declare a variable with a floating point number value that is not a whole number and return it
-
+let myvariable3 = 1.5;
+return myvariable3;
 }
 
 function lvl1exercise4 () {
   // Declare a variable and with a string value as "Hello World!" and return it
-
+let myvariable4 = "Hello World!";
+return myvariable4;
 }
 
 function lvl1exercise5 () {
   // Declare a variable and with an array value containing the string "Hello World!" and the number 4 and return it
+let myvariable5 ["Hello World!" + 4];
+return myvariable5;
 
 }
 
